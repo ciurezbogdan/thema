8d8faafdc092552b694bbd8ca661a9bc71bf750a
_ciurez_bogdan <ciurezbogdan@hotmail.com>
2021-01-27 11:11:18 +0200

    theme

--- main_hw2.py
+++ main_hw2.py
@@ -2,10 +2,10 @@ def func(*args, **kwargs):  # am folosit si kwargs intrucat avem un param key:va
     el_sum = 0
     val_sum = 0
     for i in args:
-        if type(i) == int:
+        if type(i) == int or type(i) == float:
             el_sum += i
     for k in kwargs.values():  # pot adauga sau nu param key:value
-        if type(k) == int:
+        if type(k) == int or type(k) == float:
             val_sum += k
     return el_sum  # + val_sum
 
@@ -13,28 +13,23 @@ def func(*args, **kwargs):  # am folosit si kwargs intrucat avem un param key:va
 def adun_n(n):
     if n == 0:
         return 0
-    else:
-        return n + adun_n(n - 1)
+    return n + adun_n(n - 1)
 
 
 def adun_p(n):
     if n == 0:
         return 0
-    else:
-        if n % 2 != 0:
-            return adun_p(n - 1)
-        else:
-            return n + adun_p(n - 1)
+    if n % 2 != 0:
+        return adun_p(n -1)
+    return n + adun_p(n - 1)
 
 
 def adun_i(n):
     if n == 0:
         return 0
-    else:
-        if n % 2 == 0:
-            return adun_i(n - 1)
-        else:
-            return n + adun_i(n - 1)
+    if n % 2 == 0:
+        return adun_i(n - 1)
+    return n + adun_i(n - 1)
 
 
 def sume(n):
@@ -43,8 +38,7 @@ def sume(n):
     print(f'Suma nr impare de la 0 l la {n} este: ', adun_i(n))
 
 
-sume(8)
-
+print(sume(8))
 
 def ret_int():
     n = input('Introduceti un nr:')
@@ -64,3 +58,25 @@ print(func(2, 4, 'abc', param_1=2))
 
 
 print(ret_int())
+
+# solutia D-ului Botond: am inteles-o dar nu as fi reusit sa o fac
+# am pastrat si nr initial
+def tup_sum(n, s=0, p=0, i=0):
+
+    if n == 0:
+        return 0, s, p, i
+    else:
+        if n % 2 == 0:
+            p = p + n
+        else:
+            i = i + n
+
+    rez = tup_sum(n-1, s, p, i)
+    s = n+rez[1]
+    p = rez[2]
+    i = rez[3]
+    return n, s, p, i
+
+
+
+print(tup_sum(8))
